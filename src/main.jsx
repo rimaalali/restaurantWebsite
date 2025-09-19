c220dde3f13d064fcfb5dabb2f51e89091c8fb96
rimaalali <merela349@gmail.com>
2025-09-20 02:10:16 +0300

    back

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
-import {createHashRouter,RouterProvider} from "react-router-dom" ;
+import {createBrowserRouter,RouterProvider} from "react-router-dom" ;
 import './index.css'
 import Home from './pages/Home/Home';
 import Sweet from './pages/Sweet/Sweet';
@@ -12,7 +12,7 @@ import Buy from './pages/Buy/Buy';
 import Bill from './pages/Bill/Bill';
 import Dashbord from './pages/DashBord/Dashbord';
 import Admin from './pages/Admin/Admin'
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>,
